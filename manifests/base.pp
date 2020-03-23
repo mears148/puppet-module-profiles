cced91bba5d80a758cea1208c2d73bd35596a848
mears148 <johnsmears@gmail.com>
2020-03-23 13:27:53 -0400

    initial commit

---
+++ manifests/base.pp
@@ -0,0 +1,11 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include profiles::base
+class profiles::base {
+
+  include security
+
+}
